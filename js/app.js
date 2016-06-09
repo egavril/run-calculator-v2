307bf67bf2c3fc11cef5ae63a24fb577b0861d33
Tyler van der Hoeven <hi@tyvdh.com>
2016-06-08 22:21:10 -0400

    made it work!!

--- js/app.js
+++ js/app.js
@@ -3,39 +3,34 @@ var hoursInput = document.getElementById('hoursInput');
 var minutesInput = document.getElementById('minutesInput');
 var secondsInput = document.getElementById('secondsInput');
 var calculateButton = document.getElementById('calculateButton');
-var paceOutput = document.getElementById('pace');
+var paceInput = document.getElementById("paceInput")
 
-
-// calculateButton.addEventListener('click', function() {
-
-document.getElementById("distanceInput").addEventListener("keyup", calculateStats);
-
-document.getElementById("hoursInput").addEventListener("keyup", calculateStats);
-
-document.getElementById("minutesInput").addEventListener("keyup", calculateStats);
-
-document.getElementById("secondsInput").addEventListener("keyup", calculateStats);
-
-document.getElementById("paceInput").addEventListener("keyup", calculateStats);
+distanceInput.addEventListener("keyup", calculateStats);
+hoursInput.addEventListener("keyup", calculateStats);
+minutesInput.addEventListener("keyup", calculateStats);
+secondsInput.addEventListener("keyup", calculateStats);
+paceInput.addEventListener("keyup", calculateStats);
 
 function calculateStats() {
 
-	//convert input strings to number values
-	var miles = parseFloat(distanceInput.value); 
-	var	hours = parseFloat(hoursInput.value);
-	var	minutes = parseFloat(minutesInput.value);
-	var	seconds = parseFloat(secondsInput.value);
+	// convert input strings to number values
+	var miles = Number(distanceInput.value);
+	var	hours = Number(hoursInput.value);
+	var	minutes = Number(minutesInput.value);
+	var	seconds = Number(secondsInput.value);
 
-	//convert to minutes per mile
+  if (!miles)
+    return paceInput.value = 0;
+
+	// convert to minutes per mile
 	var totalMinutes = ((hours * 60) + minutes + (seconds / 60)),
-			pace = (totalMinutes / miles), 
-			paceMinutes = Math.floor(pace), 
+			pace = (totalMinutes / miles),
+			paceMinutes = Math.floor(pace),
 			paceSeconds = Math.round((pace - paceMinutes) * 60);
 
 	if (paceSeconds < 10) {
 		paceSeconds = '0' + paceSeconds;
 	}
-	
-	document.getElementById('paceInput').innerHTML = paceMinutes + ':' + paceSeconds;
 
-};
+	return paceInput.value = paceMinutes + ':' + paceSeconds;
+}
